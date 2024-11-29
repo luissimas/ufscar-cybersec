e2084cce96207ccc272a59ed7fe3a9af6b66a59b
Luís Simas <luissimas@protonmail.com>
2024-11-29 00:01:37 -0300

    feat: cr0wnair almost there

---
+++ cr0wnair/source/config.js
@@ -0,0 +1,13 @@
+let config = {};
+
+config.pubkey = `-----BEGIN PUBLIC KEY-----
+???
+-----END PUBLIC KEY-----`;
+
+config.privkey = `-----BEGIN RSA PRIVATE KEY-----
+???
+----END RSA PRIVATE KEY-----`;
+
+config.flag = "union{I_<3_JS0N_4nD_th1ngs_wr4pp3d_in_JS0N}"
+
+module.exports = config;
